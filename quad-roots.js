532738d9e1ad967cfd17a4ede764f0702729061d
YousafAzabi <joeazabi@gmail.com>
2018-11-28 15:13:20 +0000

    Code for throw error if a=0

--- quad-roots.js
+++ quad-roots.js
@@ -1,9 +1,13 @@
 //module to calcualte the roots of a Quadratic Equation
 exports.find = (a, b, c) => {
-  
-  let sqrt = Math.pow(b, 2) - (4 * a * c); //calculate the value under square-root
-  let term1 = (-b) / (2 * a);
-  let term2 = Math.sqrt(Math.abs(sqrt)) / (2 * a);
+
+  if(a == 0) { //throw error if quadratic term coefficient is zero
+    throw "ERROR: coefficient of quadratic term cannot be 0.";
+  }
+
+  const sqrt = Math.pow(b, 2) - (4 * a * c); //calculate the value under square-root
+  const term1 = (-b) / (2 * a);
+  const term2 = Math.sqrt(Math.abs(sqrt)) / (2 * a);
 
   if (sqrt < 0) { // check if sqaure-root is negative
     return [term1 + ' + ' + term2 + ' j ' , term1 + ' - ' + term2 + ' j ' ]; //return complex roots
