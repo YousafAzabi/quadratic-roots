c91b1fa9a9baf85899ce5127f3b301344d087120
YousafAzabi <joeazabi@gmail.com>
2018-11-28 15:09:33 +0000

    Added comments and README.md

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # quadratic-roots
 Finding roots of quadratic equation
+
+The file `quad-roots.js` is a JS module can be implemented to find the roots of a quadratic equation.
+The `quad-roots` has a function `find` which accepts three input values (a, b and c), the coefficients of the quadratic equation:
+
+```
+ax^2 + bx + c = 0
+```
--- quad-roots.js
+++ quad-roots.js
@@ -1,12 +1,12 @@
 //module to calcualte the roots of a Quadratic Equation
 exports.find = (a, b, c) => {
-
-  let sqrt = Math.pow(b, 2) - (4 * a * c);
+  
+  let sqrt = Math.pow(b, 2) - (4 * a * c); //calculate the value under square-root
   let term1 = (-b) / (2 * a);
   let term2 = Math.sqrt(Math.abs(sqrt)) / (2 * a);
 
-  if (sqrt < 0) {
-    return [term1 + ' + ' + term2 + ' j ' , term1 + ' - ' + term2 + ' j ' ];
+  if (sqrt < 0) { // check if sqaure-root is negative
+    return [term1 + ' + ' + term2 + ' j ' , term1 + ' - ' + term2 + ' j ' ]; //return complex roots
   }
-  return [(term1 + term2), (term1 - term2)]; 
+  return [(term1 + term2), (term1 - term2)]; //return real number roots
 };
